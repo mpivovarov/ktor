b644856ed754d04232688b0ff7ea9d35272463de
MP <michail.michail@gmail.com>
2020-04-30 10:10:06 +0300

    async

--- src/route/NewsController.kt
+++ src/route/NewsController.kt
@@ -1,23 +1,33 @@
 package my.ktor.route
 
 
+import io.ktor.application.ApplicationCall
 import io.ktor.application.call
 import io.ktor.locations.*
 import io.ktor.request.receive
 import io.ktor.response.respond
 import io.ktor.routing.Route
+import kotlinx.coroutines.*
 import model.News
 import my.ktor.dao.NewsDAO
 import my.ktor.model.SuccessResponse
 import org.kodein.di.Kodein
 import org.kodein.di.generic.instance
 import java.time.Instant
+import java.util.*
+import java.util.concurrent.Executors
 
 @Location("/news") class NewsListLoc
 @Location("/news") class NewsLoc//(val id: Long? = null, val title: String, val text: String)
 @Location("/news/{id}") data class NewsIdLoc(val id: Long)
+@Location("/news/generate/{count}") data class NewsGenLoc(val count: Int)
+
+//val execPool: ExecutorCoroutineDispatcher by lazy {
+//    Executors.newFixedThreadPool(4).asCoroutineDispatcher()
+//}
 
 fun Route.newsHandler(kodein: Kodein) {
+
     val dao by kodein.instance<NewsDAO>()
 
     get<NewsListLoc> {
@@ -50,4 +60,18 @@ fun Route.newsAdminHandler(kodein: Kodein) {
         val result = dao.delete(dto.id)
         call.respond(SuccessResponse(result))
     }
+
+    get<NewsGenLoc> { dto ->
+        val gen = async { generateNews(dto.count, dao) }
+        call.respond(SuccessResponse("generated"))
+    }
+
+}
+
+private /*suspend*/ fun generateNews(count: Int, dao: NewsDAO) /*= withContext(execPool)*/ {
+    println("begin generate")
+    (1..count).forEach {
+        dao.create(News(null, Instant.now(), UUID.randomUUID().toString(), UUID.randomUUID().toString()))
+    }
+    println("end generate")
 }
